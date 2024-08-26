0ad956d1579501e71f174eeaa3a0d0ea7e7356d4
Ibrahim Ansari <ansari.ibrahim1@gmail.com>
2024-08-27 00:05:47 +0530

    Rewrite /givecreativeplot (untested)

--- src/main/java/org/mythicmc/mythicstore/command/CreativePlotCommand.java
+++ src/main/java/org/mythicmc/mythicstore/command/CreativePlotCommand.java
@@ -1,12 +1,15 @@
 package org.mythicmc.mythicstore.command;
 
+import net.luckperms.api.LuckPermsProvider;
+import net.luckperms.api.context.ImmutableContextSet;
+import net.luckperms.api.node.Node;
+import net.luckperms.api.query.QueryOptions;
 import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.mythicmc.mythicstore.MythicStore;
-import org.mythicmc.mythicstore.util.CreativePlotUtils;
 
 public class CreativePlotCommand implements CommandExecutor {
     private final MythicStore plugin;
@@ -18,13 +21,41 @@ public class CreativePlotCommand implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         if (args.length != 1) return false;
-        Player p = Bukkit.getPlayer(args[0]);
+        Player p = Bukkit.getPlayerExact(args[0]);
         if (p == null || !p.isOnline()) {
             sender.sendMessage("§4That player is not online");
             return true;
         }
-        CreativePlotUtils.increasePlotAmount(p, plugin);
-        sender.sendMessage("§aSuccessfully gave " + p.getName() + " a creative plot");
+
+        int maxPlots = plugin.getConfig().getInt("givecreativeplot.max-plots");
+        int minPlots = plugin.getConfig().getInt("givecreativeplot.min-plots");
+
+        var lp = LuckPermsProvider.get();
+        var user = lp.getPlayerAdapter(Player.class).getUser(p);
+        var context = ImmutableContextSet.builder()
+                .add("server", "creative")
+                .add("world", "creative")
+                .build();
+        // Find all nodes which match this context
+        var nodes = user.resolveInheritedNodes(QueryOptions.contextual(context));
+
+        for (int i = maxPlots - 1; i >= minPlots; i--) {
+            int finalI = i;
+            var node = nodes.stream()
+                    .filter(n -> n.getKey().equalsIgnoreCase("plots.plot." + finalI))
+                    .findFirst();
+            if (node.isPresent()) {
+                if (i > minPlots) {
+                    user.data().remove(node.get());
+                }
+                user.data().add(Node.builder("plots.plot." + (i + 1)).context(context).build());
+                lp.getUserManager().saveUser(user).thenAcceptAsync(success -> sender
+                        .sendMessage("§aSuccessfully gave " + p.getName() + " a creative plot"));
+                return true;
+            }
+        }
+
+        sender.sendMessage("§4That player already has the maximum number of plots");
         return true;
     }
 }
